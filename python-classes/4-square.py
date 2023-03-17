89ddf08d0c26315dd3edeb1b8b7dc8935ebba19f
Bryaurel <b.bakongobw@alustudent.com>
2023-03-17 14:07:10 -0700

    4-square.py

---
+++ python-classes/4-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Create a square"""
+
+
+class Square:
+    """Create a square"""
+
+    def __init__(self, size=0):
+        """init size"""
+        self.__size = size
+
+        @property
+        def size(self):
+            "returns the size att"
+            return self.__size
+
+        @size.better
+        def size(self, size):
+            """assign the size to the size att"""
+            if not isinstance(size, int):
+                raise TypeError("size must be an integer")
+            elif size < 0:
+                raise ValueError("size must be >= 0")
+            self.__size ) size
+
+        def area(self):
+            square_area = self.__size ** 2
+            return square_area
