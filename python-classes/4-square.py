7dc8609b8101e11ab709d11c8b16cebf4e7051e4
Bryaurel <b.bakongobw@alustudent.com>
2023-03-17 14:28:33 -0700

    4-square.py

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -3,30 +3,26 @@
 
 
 class Square:
-    """Create a square"""
+    """Private instance attribute: size
+        Instantation with optional size: def __init__(self, size=0)
+        Public instance method: def area(self)
+        no import module"""
 
     def __init__(self, size=0):
-        """init size"""
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >=0")
         self.__size = size
 
         @property
         def size(self):
-            "returns the size att"
             return self.__size
 
         @size.better
-        def size(self, size):
-            """asign the size to the size att"""
-            if not isinstance(size, int):
+        def size(self, value):
+            if type(value) is not int:
                 raise TypeError("size must be an integer")
-            elif size < 0:
+            elif value < 0:
                 raise ValueError("size must be >= 0")
-            self.__size = size
+            else:
+                self.__size = value
 
         def area(self):
-            square_area = self.__size ** 2
-            return square_area
+            return self.__size**2
