50b5d5f0ae3356e896ef7adf4659d0514ce9c2d5
Bryaurel <b.bakongobw@alustudent.com>
2023-04-07 01:07:23 +0000

    0-hbtn_status.py

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-"""Python script that fetches https://alu-intranet.hbtn.io/status
-import urllib.request"""
+"""Python script that fetches https://alu-intranet.hbtn.io/status"""
+
+import urllib.request
 
 if __name__ == "__main__":
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
