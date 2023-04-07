8d341c061eb94da5836cfec58016c2ea7f1c485b
Bryaurel <b.bakongobw@alustudent.com>
2023-04-07 01:22:16 +0000

    3-error_code.py

--- python-network_1/3-error_code.py
+++ python-network_1/3-error_code.py
@@ -1,17 +1,6 @@
 #!/usr/bin/python3
-"""Python script that takes in a URL, sends a request to the URL and displays the body of the response (decoded in utf-8)"""
-from urllib import request, error
-import sys
+"""Python script that takes in a URL, sends a request to the URL"""
 
-
-if __name__ == "__main__":
-    try:
-        with request.urlopen(sys.argv[1]) as response:
-            body = response.read()
-            print(body.decode('utf-8'))
-    except error.HTTPError as err:
-        print('Error code: {}'.format(err.code))n3
-"""Python script that takes in a URL, sends a request to the URL and displays the body of the response (decoded in utf-8)"""
 from urllib import request, error
 import sys
 
