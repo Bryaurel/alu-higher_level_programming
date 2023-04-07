f37f5252472b254e43de1ee71c38789760be2db4
Bryaurel <b.bakongobw@alustudent.com>
2023-04-07 01:30:29 +0000

    7-error_code.py

--- python-network_1/7-error_code.py
+++ python-network_1/7-error_code.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""Python script that takes in a URL, sends a request to the URL and displays the body of the response"""
+"""Takes in a URL, sends a request to the URL and displays the body"""
+
 import requests
 import sys
 
