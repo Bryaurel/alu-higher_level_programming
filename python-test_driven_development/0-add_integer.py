305b240b72611ddc240d536dbc1ded6fb64b5d0a
Bryaurel <b.bakongobw@alustudent.com>
2023-04-25 13:56:41 -0700

    0-add_integer.py

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""adds 2 integers"""
+
+def add_interger(a, b=98):
+    """a function that adds two integers
+    Args:
+    a: must be an integer or float
+    b: must be an integer or float
+    """
+    if type(a) == float or type(b) == float:
+        a = int(a)
+        b = int(b)
+
+    if type(a) != int:
+        raise TypeError("a must be an integer")
+    if type(b) != int:
+        raise TypeError("b must be an integer")
+
+    return a + b
